8b0333416863522d656ddebac2ed695b328c3a74
rezucch1 <rebecca.zucchi@mail.polimi.it>
2026-05-14 18:19:28 +0200

    fixing runge kutta and multipole

--- src/multipole.cpp
+++ src/multipole.cpp
@@ -1,17 +1,17 @@
 #include "multipole.hpp"
-constexpr unsigned int threshold = 5;
+constexpr unsigned int threshold = 5; 
 
 Multipole::Multipole(const Multipole *parent, std::vector<Particle>::iterator &particles_begin, std::vector<Particle>::iterator &particles_end, const Tensor &a, const Tensor &b)
 : parent(parent)
 {
     const Tensor c = (a+b)/2;
-    unsigned int counter[4]; //2D
+    unsigned int counter[4]; //2D, dim
 
     for(int i=0; i<4; i++){
             counter[i]=0; //initialize all counter to 0
     }
 
-    std::vector<Particle>::iterator children_id[5];
+    std::vector<Particle>::iterator children_id[5]; //2^dim + 1 divisione sottovettori punti estremi dei sottovettori
     int i;
     for(std::vector<Particle>::iterator p = particles_begin; p < particles_end; p++){
         i = p->get_position()[0] > c[0]; //c[0] is the x position of c
@@ -40,7 +40,7 @@ Multipole::Multipole(const Multipole *parent, std::vector<Particle>::iterator &p
         for(i=0; i<4; i++){
             counter[i]=0; //initialize all counter to 0
         }
-        for(std::vector<Particle>::iterator p = particles_begin; p < particles_end; p++){
+        for(int p = 0; p < particles_end - particle_begin; p++){
             i = p->get_position()[0] > c[0]; //c[0] is the x position of c
             //if particle p is on c's left, i=0; if particle p is on c's right, i=1;
             i += 2 * (p->get_position()[1] > c[1]); //if TRUE, i+=10; if FALSE i=i;
@@ -62,21 +62,21 @@ Multipole::Multipole(const Multipole *parent, std::vector<Particle>::iterator &p
         if( counter[i] > threshold) //non è leaf
         {
             if(i & 1 == 0){//if the least significant bit=0
-                a2[1]=a[1];
-                b2[1]=c[1];
-            }
-            else{
-                a2[1]=c[1];
-                b2[1]=b[1];
-            }
-
-            if(i & 2 == 0){ //if the second least significant bit=0
                 a2[0]=a[0];
                 b2[0]=c[0];
             }
             else{
                 a2[0]=c[0];
                 b2[0]=b[0];
+            }
+
+            if(i & 2 == 0){ //if the second least significant bit=0
+                a2[1]=a[1];
+                b2[1]=c[1];
+            }
+            else{
+                a2[1]=c[1];
+                b2[1]=b[1];
             }
 
             children.push_back(Multipole(this, children_id[i], children_id[i + 1] , a2, b2));
--- src/runge_kutta_4.cpp
+++ src/runge_kutta_4.cpp
@@ -18,17 +18,14 @@ void RungeKutta4::integrate(SystemUpdateMethod *method, std::vector<Particle> &p
 
   // stage 1
   std::copy(particles.begin(), particles.end(), std::back_inserter(particles_k));
+
+
   for (unsigned int i = 0; i < particles.size(); ++i){
-    assert(&(particles[i]) != &(particles_k[i]));
-    assert(&(particles[i].get_position()) != &(particles_k[i].get_position()));
-    assert(particles[i].get_position() == particles_k[i].get_position());
-    assert(&(particles[i].get_velocity()) != &(particles_k[i].get_velocity()));
-    assert(particles[i].get_velocity() == particles_k[i].get_velocity());
+    velocity_k[i] = particles[i].get_velocity();
   }
 
   acceleration_k = method->update(particles_k);
   for (unsigned int i = 0; i < particles.size(); ++i){
-    velocity_k[i] = particles[i].get_velocity();
 
     accelerations_sum[i] = acceleration_k[i];
     assert(!std::isnan(accelerations_sum[i].squared_norm()));
@@ -39,69 +36,54 @@ void RungeKutta4::integrate(SystemUpdateMethod *method, std::vector<Particle> &p
   // stage 2
   std::copy(particles.begin(), particles.end(), particles_k.begin());
   for (unsigned int i = 0; i < particles.size(); ++i){
-    assert(&(particles[i]) != &(particles_k[i]));
-    assert(&(particles[i].get_position()) != &(particles_k[i].get_position()));
-    assert(particles[i].get_position() == particles_k[i].get_position());
-    assert(&(particles[i].get_velocity()) != &(particles_k[i].get_velocity()));
-    assert(particles[i].get_velocity() == particles_k[i].get_velocity());
-
     position(particles_k[i]) += delta_t / 2 * velocity_k[i];
+  }
+
+  for (unsigned int i = 0; i < particles.size(); ++i){
+    velocity_k[i] = particles[i].get_velocity() + delta_t / 2 * acceleration_k[i];
   }
 
   acceleration_k = method->update(particles_k);
   for (unsigned int i = 0; i < particles.size(); ++i){
-    velocity_k[i] = particles[i].get_velocity() + delta_t / 2 * acceleration_k[i];
-
     accelerations_sum[i] += 2*acceleration_k[i];
-    assert(!std::isnan(accelerations_sum[i].squared_norm()));
     velocity_sum[i] += 2*velocity_k[i];
   }
 
   // stage 3
   std::copy(particles.begin(), particles.end(), particles_k.begin());
   for (unsigned int i = 0; i < particles.size(); ++i){
-    assert(&(particles[i]) != &(particles_k[i]));
-    assert(&(particles[i].get_position()) != &(particles_k[i].get_position()));
-    assert(particles[i].get_position() == particles_k[i].get_position());
-    assert(&(particles[i].get_velocity()) != &(particles_k[i].get_velocity()));
-    assert(particles[i].get_velocity() == particles_k[i].get_velocity());
-
     position(particles_k[i]) += delta_t / 2 * velocity_k[i];
+  }
+
+  for (unsigned int i = 0; i < particles.size(); ++i){
+    velocity_k[i] = particles[i].get_velocity() + delta_t / 2 * acceleration_k[i];
   }
 
   acceleration_k = method->update(particles_k);
   for (unsigned int i = 0; i < particles.size(); ++i){
-    velocity_k[i] = particles[i].get_velocity() + delta_t / 2 * acceleration_k[i];
 
     accelerations_sum[i] += 2*acceleration_k[i];
-    assert(!std::isnan(accelerations_sum[i].squared_norm()));
     velocity_sum[i] += 2*velocity_k[i];
   }
 
   // stage 4
   std::copy(particles.begin(), particles.end(), particles_k.begin());
   for (unsigned int i = 0; i < particles.size(); ++i){
-    assert(&(particles[i]) != &(particles_k[i]));
-    assert(&(particles[i].get_position()) != &(particles_k[i].get_position()));
-    assert(particles[i].get_position() == particles_k[i].get_position());
-    assert(&(particles[i].get_velocity()) != &(particles_k[i].get_velocity()));
-    assert(particles[i].get_velocity() == particles_k[i].get_velocity());
-
     position(particles_k[i]) += delta_t * velocity_k[i];
+  }
+  for (unsigned int i = 0; i < particles.size(); ++i){
+    velocity_k[i] = particles[i].get_velocity() + delta_t * acceleration_k[i];
   }
 
   acceleration_k = method->update(particles_k);
-  for (unsigned int i = 0; i < particles.size(); ++i){
-    velocity_k[i] = particles[i].get_velocity() + delta_t * acceleration_k[i];
 
+  for (unsigned int i = 0; i < particles.size(); ++i){
     accelerations_sum[i] += acceleration_k[i];
-    assert(!std::isnan(accelerations_sum[i].squared_norm()));
     velocity_sum[i] += velocity_k[i];
   }
 
   // final next velocity and position
   for (unsigned int i = 0; i < particles.size(); ++i){
-    assert(!std::isnan(accelerations_sum[i].squared_norm()));
     velocity(particles[i]) += delta_t / 6 * accelerations_sum[i];
     position(particles[i]) += delta_t / 6 * velocity_sum[i];
   }
